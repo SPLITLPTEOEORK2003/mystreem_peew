9edd48c3772e5381b1f16ae4ab6da5562a2f9abd
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-24 13:09:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'setg')) {
+  if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
       message.channel.send(`✅`)
   } else 
